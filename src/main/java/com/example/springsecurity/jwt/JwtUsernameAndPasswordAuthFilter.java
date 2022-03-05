efc8789daab6ef7862959431ac2af6321ff1f634
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-03-05 13:48:02 +0300

    feat: send generated token on successful auth

--- src/main/java/com/example/springsecurity/jwt/JwtUsernameAndPasswordAuthFilter.java
+++ src/main/java/com/example/springsecurity/jwt/JwtUsernameAndPasswordAuthFilter.java
@@ -1,15 +1,22 @@
 package com.example.springsecurity.jwt;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -36,5 +43,20 @@ public class JwtUsernameAndPasswordAuthFilter extends UsernamePasswordAuthentica
         } catch (IOException e) {
             throw  new RuntimeException(e);
         }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        String key = "asdasdasdadadadasdadasdadasdadsasdasdasdpoij";
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
     }
 }
