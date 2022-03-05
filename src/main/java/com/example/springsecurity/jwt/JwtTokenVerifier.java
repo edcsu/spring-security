0373114a5c8170bbd689576f1e09f40264e10df9
Sewannonda Keith Edwin <skeith696@gmail.com>
2022-03-05 14:37:26 +0300

    refactor: JWT token verifier filter

---
+++ src/main/java/com/example/springsecurity/jwt/JwtTokenVerifier.java
@@ -0,0 +1,65 @@
+package com.example.springsecurity.jwt;
+
+import com.google.common.base.Strings;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JwtTokenVerifier extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) {
+        String authHeader = request.getHeader("Authorization");
+        if(Strings.isNullOrEmpty(authHeader) || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        String token  = authHeader.replace("Bearer ", "");
+
+        try {
+            String secretKey = "asdasdasdadadadasdadasdadasdadsasdasdasdpoij";
+
+            Jws<Claims> claimsJws = Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                    .build()
+                    .parseClaimsJws(token);
+
+            Claims body = claimsJws.getBody();
+
+            String subject = body.getSubject();
+
+            var authorities = (List<Map<String, String>>) body.get("authorities");
+
+            Set<SimpleGrantedAuthority> simpleGrantedAuthorities = authorities.stream()
+                    .map(authority -> new SimpleGrantedAuthority(authority.get("authority")))
+                    .collect(Collectors.toSet());
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    subject,
+                    null,
+                    simpleGrantedAuthorities
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } catch (JwtException e) {
+            throw new IllegalStateException(String.format("Bad token: %s", token));
+        }
+    }
+}
